ab179215eb5cfd2d531446da1c3a9e1c35564d45
DoughnutsNCookies <105269435+DoughnutsNCookies@users.noreply.github.com>
2022-07-04 20:30:57 +0800

    Added libft

---
+++ libft/ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schuah <schuah@student.42.kl.edu.my>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 19:26:25 by schuah            #+#    #+#             */
+/*   Updated: 2022/07/04 19:28:44 by schuah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *src)
+{
+	char	*copy;
+	int		counter;
+	int		index;
+
+	counter = 0;
+	while (src[counter] != '\0')
+		counter++;
+	copy = malloc((counter * 1) + 1);
+	if (copy == NULL)
+		return (0);
+	index = 0;
+	while (src[index] != '\0')
+	{
+		copy[index] = src[index];
+		index++;
+	}
+	copy[index] = '\0';
+	return (copy);
+}
---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schuah <schuah@student.42.kl.edu.my>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 15:24:02 by schuah            #+#    #+#             */
+/*   Updated: 2022/07/04 15:46:18 by schuah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (i < (size - 1) && src[i] != '\0')
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
