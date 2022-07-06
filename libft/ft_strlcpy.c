9000737725c3be5e6276c31d435b8582846512d9
DoughnutsNCookies <105269435+DoughnutsNCookies@users.noreply.github.com>
2022-07-06 22:58:48 +0800

    Update ft_strlcpy.c

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -16,14 +16,11 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
-	i = 0;
+	i = -1;
 	if (size > 0)
 	{
-		while (i < (size - 1) && src[i] != '\0')
-		{
+		while (++i < (size - 1) && src[i] != '\0')
 			dst[i] = src[i];
-			i++;
-		}
 		dst[i] = '\0';
 	}
 	return (ft_strlen(src));
