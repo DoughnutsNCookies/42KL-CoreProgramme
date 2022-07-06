de2b8c16f4ffead478ef13119f006659c12f02c9
DoughnutsNCookies <105269435+DoughnutsNCookies@users.noreply.github.com>
2022-07-06 23:01:40 +0800

    Update ft_strdup.c

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -24,12 +24,9 @@ char	*ft_strdup(const char *src)
 	copy = malloc((counter * 1) + 1);
 	if (copy == NULL)
 		return (0);
-	index = 0;
-	while (src[index] != '\0')
-	{
+	index = -1;
+	while (src[++index] != '\0')
 		copy[index] = src[index];
-		index++;
-	}
 	copy[index] = '\0';
 	return (copy);
 }
