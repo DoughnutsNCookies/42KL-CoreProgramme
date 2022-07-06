75b79725776fc94073784cebf5690c39214a8829
DoughnutsNCookies <105269435+DoughnutsNCookies@users.noreply.github.com>
2022-07-06 23:01:02 +0800

    Update ft_striteri.c

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -18,10 +18,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 
 	if (!s)
 		return ;
-	i = 0;
-	while (s[i] != '\0')
-	{
+	i = -1;
+	while (s[++i] != '\0')
 		(*f)(i, &s[i]);
-		i++;
-	}
 }
