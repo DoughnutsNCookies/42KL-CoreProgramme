4f87624c56e92ff761eef57be59d8d6ec3786bc5
DoughnutsNCookies <105269435+DoughnutsNCookies@users.noreply.github.com>
2022-07-07 15:42:02 +0800

    Updates and bug fix

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42.kl.edu.my>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 16:38:54 by schuah            #+#    #+#             */
-/*   Updated: 2022/07/04 17:08:14 by schuah           ###   ########.fr       */
+/*   Updated: 2022/07/07 10:25:38 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
 
+	i = -1;
 	if (n == 0)
 		return (0);
-	i = -1;
 	while (++i < n)
 		if (*(unsigned char *)(s1 + i) != *(unsigned char *)(s2 + i))
 			return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
